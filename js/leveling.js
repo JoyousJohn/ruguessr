56579e99620e3fbc2b1efc204d876a57e11b50fc
John <mrjohnishappy@gmail.com>
2024-04-27 17:04:01 -0400

    Smoothly update each bar lvl upgrade

--- js/leveling.js
+++ js/leveling.js
@@ -45,20 +45,29 @@ function updateLevel(useFake) {
 
     console.log(levelCalc)
 
+    let levelDiff = 0
     if (useFake === true) {
+        console.log('currentLevel: ' + fakeLvl + ' newLevel: ' + levelCalc)
+        levelDiff = levelCalc - fakeLvl
         fakeLvl = levelCalc
     } else {
+        console.log('currentLevel: ' + level + ' newLevel: ' + levelCalc)
+        levelDiff = levelCalc - level
         level = levelCalc
     }
+
+    console.log('levelDiff: ', levelDiff)
+
+    return levelDiff
 
 }
 
 function addXp(toAdd, useFake, updateBar=true) {
     if (useFake === true) {
-        console.log('adding ' + toAdd + ' fake xp')
+        // console.log('adding ' + toAdd + ' fake xp')
         setXp(fakeXp+toAdd, useFake, updateBar=updateBar)
     } else {
-        console.log('adding ' + toAdd + ' real xp')
+        // console.log('adding ' + toAdd + ' real xp')
         setXp(xp+toAdd, useFake, updateBar)
     }
 }
@@ -67,17 +76,17 @@ function setXp(newXp, useFake, updateBar=true) {
 
     if (useFake === true) {
         fakeXp = newXp
-        console.log('setting fake xp to ' + newXp)
+        // console.log('setting fake xp to ' + newXp)
     } else {
         xp = newXp
-        console.log('setting real xp to ' + newXp)
+        // console.log('setting real xp to ' + newXp)
     }
 
-    updateLevel(useFake)
+    const levelDiff = updateLevel(useFake)
 
     if (updateBar === true) {
-        console.log('set xp triggered updateprogressbar')
-        updateProgressBar(useFake)
+        // console.log('set xp triggered updateprogressbar')
+        updateProgressBar(useFake, levelDiff)
     }
 
     localStorage.setItem('xp', xp);
@@ -91,7 +100,11 @@ $(document).ready(function() {
 
 })
 
-function updateProgressBar(useFake) {
+function barTransitionSec() {
+    return parseFloat($('.level-progress').css('transition-duration'));
+}
+
+function updateProgressBar(useFake, levelDiff=0) {
 
     let useLvl, useXp
 
@@ -103,15 +116,68 @@ function updateProgressBar(useFake) {
         useXp = xp
     }
 
-    $('.xp-current').text(useLvl)
-    $('.xp-needed').text(useLvl+1)
-
     const neededXp = xpReq[useLvl + 1] - xpReq[useLvl]
     const hasXp = useXp - xpReq[useLvl]
     const progressPercent = hasXp/neededXp*100
-    $('.level-progress').width(progressPercent + '%')
 
-    console.log('updating progress bar with ' + progressPercent + ' percent')
+
+    console.log('levelDiff input: ', levelDiff)
+
+    if (levelDiff > 0) {
+
+        const currBarTransition = barTransitionSec()
+        const tempBarTransition = currBarTransition/levelDiff
+        $('.level-progress').css('transition-duration', tempBarTransition + 's')
+
+        $('.level-progress').width('100%')
+
+        const oldLvl = useLvl - levelDiff
+
+        for (let loopLvl = 0; loopLvl < levelDiff+1; loopLvl++) {
+
+            // $('.level-progress').css('transition-duration', '0')
+            // $('.level-progress').width('0%')
+
+            console.log('timeout to get to lvl ' + (oldLvl + loopLvl) + ': ' + tempBarTransition * loopLvl * 1000 + 'ms')
+
+            setTimeout(() => {
+
+                if (loopLvl !== 0) { // Don't reset current level progress to 0
+                    $('.level-progress').css('transition-duration', '0s')
+                    $('.level-progress').width('0%')
+                }
+
+                setTimeout(() => { // Requires a delay to properly finish animating to width 0% first
+                    console.log('setting .xp-current lvl to: ' + (oldLvl + loopLvl));
+                    $('.level-progress').css('transition-duration', tempBarTransition + 's');
+
+                    if (oldLvl + loopLvl === useLvl) {
+                        $('.level-progress').width(progressPercent + '%')
+                        $('.level-progress').css('transition-duration', currBarTransition + 's') // Last animation, to reset duration
+                        console.log('resetting bar trans dur to ' + currBarTransition + 's')
+                    } else {
+                        $('.level-progress').width('105%');
+                    }
+
+                    $('.xp-current').text(oldLvl + loopLvl);
+                    $('.xp-needed').text(oldLvl + loopLvl + 1);
+                }, 1); 
+
+            }, tempBarTransition * loopLvl * 1000);
+
+        }
+
+
+    // Else if level doesn't change
+    } else {
+
+        $('.xp-current').text(useLvl)
+        $('.xp-needed').text(useLvl+1)
+        $('.level-progress').width(progressPercent + '%')
+        console.log('updating progress bar with ' + progressPercent + ' percent')
+
+    }
+
 
 }
 
